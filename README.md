711d2c412215f4179da3aec1542491037b722755
maoyonglong <githubMYL@163.com>
2019-12-15 18:12:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,74 @@
+# Desciption
+这是一个swiper的js动画插件，语法参考并类似[swiper.animate.js](https://www.swiper.com.cn/usage/animate/index.html)，[swiper.animate-twice](http://bbs.swiper.com.cn/forum.php?mod=viewthread&tid=328&extra=page%3D1)
+
+# Usage
+1. 载入[jquery.js](https://jquery.com/)或者[zepto.js](https://zeptojs.com/)和[swiper-effect.js](https://github.com/maoyonglong/swiper-effect)
+```html
+<script src="jquery.js"></script>
+<!-- 或者 -->
+<script src="zepto.js"></script>
+<!-- 如果使用velocity -->
+<script src="velocity.js"></script>
+<!-- 载入插件 -->
+<script src="swiper-effect.js"></script>
+```
+2. 添加动画元素  
+`eff`是默认的动画类名   
+`data-eff-in`和`data-eff-out`是控制默认的dataset。   
+dataset支持的语法是`<effect>:<args1>,<args2>,...` ，表示触发动画时调用`effect($effs, args1, args2)`，如果没有参数，直接`<effect>`   
+而其中的`<args>`可以是：  
+数组 -> `[1, 2, 3]`  
+字符串 -> `asd`, 或加上单引号`'asd'`，默认只会去掉第一层的单引号，比如，`'132'd'`就是字符串`123'd ` 
+数字 -> `123456`  
+对象 -> `duration=1000;delay=500`会被转成`{duration: 1000, delay: 5000}`   
+在
+```js
+<div class="swiper-slide">
+<p class="eff" data-eff-in="velocity:slideDown,duration=1000;delay=500" data-eff-out="velocity:slideUp,duration=1000;delay=500">内容</p>
+</div>
+```
+3. 添加effect（就是data-eff-in和data-eff-out）调用的函数
+```js
+// velocity是<effect>的名字，command和opts都是传入的参数
+// $effs表示当前slide中所有的动画元素对象
+SwiperEffect.addEffect('velocity', function ($effs, command, opts) {
+  // 这是velocity.js的1.5版本写法
+  // 在这个例子中是：
+  // command -> 进入动画是slideUp，推出动画是slideDown
+  // opts -> { duration: 1000, delay: 500 }
+  $effs.velocity(command, opts)
+})
+```
+4.  初始化时隐藏元素并在需要的时刻开始动画
+```js
+//Swiper5
+var mySwiper = new Swiper ('.swiper-container', {
+  on:{
+    init: function(){
+      swiperEffectCache(this); //缓存swiper.slides
+      swiperEffect(this); //初始化完成开始动画
+    }, 
+    slideChangeTransitionEnd: function(){ 
+      // 触发动画即调用data-eff-in和data-eff-out表示的函数
+      swiperEffect(this); //每个slide切换结束时也运行当前slide动画
+      // swiperEffect默认顺序执行in和out动画
+      // 如果需要分别控制in和out动画，需要使用swiperEffectIn和swiperEffectOut
+    } 
+  }
+})
+```
+5. 可以更改dataset的名字和动画类名
+```js
+SwiperEffect.setEffectClass('.ani') // => 类名改成ani
+SwiperEffect.setEffectDataSetName('slide')
+// => dataset改为data-slide-in和data-slide-out
+```
+6. 如果有多个Swiper实例，也需要有多个SwiperEffect是实例
+```js
+var otherSwiperEffect = SwiperEffect.factory()
+```
+7. 移除和获取effects，需要注意改变effects会影响到所有SwiperEffect实例
+```js
+SwiperEffect.getEffects()
+SwiperEffect.removeEffect('velocity')
+```
---
+++ swiper-effect.js
@@ -0,0 +1,191 @@
+/**
+ * @version 1.0.0
+ * @author maoyonglong
+ * @registory https://github.com/maoyonglong/swiper-effect.git
+ */
+var SwiperEffect = (function () {
+  if (typeof $ !== 'function') {
+    console.error('please import jquery or zepto before.')
+  }
+
+  // the classname of animation object
+  var _effClass = '.eff'
+  // dataset name
+  var _datasetName = 'eff' // => data-eff-in, data-eff-out
+
+  var _effects = {}
+
+  // doesn't use $.map becase it will concat array that I don't expect
+  // $.map([1, [2]], item => item) => [1, 2] but I need to be [1, [2]]
+  if (!('map' in Array.prototype)) {
+    Array.prototype.map = function (cb) {
+      var result = []
+      for (var i = 0, len = this.length; i < len; i++) {
+        result.push(cb.call(this[i], this[i], i))
+      }
+      return result
+    }
+  }
+
+  // parse desciption to:
+  // {
+  //   slideIn: {
+  //     effect: '',
+  //     params: []
+  //   },
+  //   slideOut: {
+  //     // ...
+  //   }
+  // }
+  function parseDesc (desc) {
+    var matchEffectParamsRe = /(.*?):(.*)/
+    var matchKeyValRe = /(.*?)=(.*)/
+    var matches = parseMatch(desc, matchEffectParamsRe, function (effect, paramsStr) {
+      var params = paramsStr.split(',')
+      // params => [ [ param1Equal1, param1Equal2, ... ], [ ... ] ]
+      params = params.map(function (param) {
+        return param.split(';')
+      })
+      // params => [ param1, param2, ... ]
+      params = params.map(function (param) {
+        var _arg = {}
+        $.each(param, function (i, paramStr) {
+          var matches = parseMatch(paramStr, matchKeyValRe, function (key, val) {
+            return [ key, val ]
+          })
+          if (matches === null) {
+            var calcVal = +paramStr
+            if (isNaN(calcVal)) {
+              _arg = paramStr
+              // remove the first layout `''`
+              if (_arg.indexOf("'") === 0) {
+                _arg = paramStr.substring(1, paramStr.length-1)
+              }
+            } else {
+              _arg = calcVal
+            }
+          } else {
+            _arg[matches[0]] = JSON.parse(matches[1].replace(/'/g, ''))
+          }
+        })
+        return _arg
+      })
+      return [ effect, params ]
+    })
+    return matches === null ? { effect: desc, params: [] } : { effect: matches[0], params: matches[1] }
+    function parseMatch (str, re, cb) {
+      var matches = str.match(re)
+      return matches === null ? null : cb.call(this, matches[1], matches[2])
+    }
+  }
+
+  function factory () {
+    var _caches = []
+    // [
+    //   // slide1
+    //   [
+    //     // eff1
+    //     {
+    //       slideIn: '',
+    //       slideOut: ''
+    //     }
+    //   ],
+    //   // ...
+    // ]
+  
+    var _$slides = null // => $(swiper.slides)
+  
+    function record ($slide, index) {
+      var _slide = _caches[index] = []
+      var $effs = $slide.find(_effClass)
+      $effs.each(function (idx, eff) {
+        var _eff = _slide[idx] = {}
+        var $eff = $(eff)
+        var dataSlideIn = $eff.data(_datasetName + '-in')
+        var dataSlideOut = $eff.data(_datasetName + '-out')
+        if (typeof dataSlideIn === 'string') _eff['slideIn'] = parseDesc(dataSlideIn)
+        if (typeof dataSlideOut === 'string') _eff['slideOut'] = parseDesc(dataSlideOut)
+      })
+    }
+  
+    function genSlideFn (direction) {
+      return function (index) {
+        var _cache = _caches[index]
+        // if exists animation
+        if (_cache.length > 0) {
+          _cache.forEach(function (c) {
+            var slideDirection = c['slide'+direction]
+            var $slide = _$slides.eq(index)
+            if (slideDirection)
+            if (slideDirection) _effects[slideDirection.effect].apply(_effects, [$slide.find(_effClass)].concat(slideDirection.params))
+          })
+        }
+      }
+    }
+  
+    var slideIn = genSlideFn('In')
+    var slideOut = genSlideFn('Out')
+  
+    function cache (index) {
+      var $activeSlide = _$slides.eq(index)
+      if (!_caches[index]) {
+        record($activeSlide, index)
+      }
+    }
+  
+    function swiperEffectCache (swiper) {
+      _$slides = $(swiper.slides)
+    }
+  
+    function genSwiperEffectFn (cb) {
+      return function (swiper) {
+        var activeIndex = swiper.activeIndex
+        cache(activeIndex)
+        cb(activeIndex)
+      }
+    }
+  
+    var swiperEffect = genSwiperEffectFn(function (activeIndex) {
+      slideIn(activeIndex)
+      slideOut(activeIndex)
+    })
+  
+    var swiperEffectIn = genSwiperEffectFn(slideIn)
+    var swiperEffectOut = genSwiperEffectFn(slideOut)
+  
+    return {
+      swiperEffectCache, swiperEffectCache,
+      swiperEffect: swiperEffect,
+      swiperEffectIn, swiperEffectIn,
+      swiperEffectOut, swiperEffectOut,
+      setEffectClass: function (effClass) {
+        _effClass = effClass
+      },
+      setEffectDataSetName: function (name) {
+        _datasetName = name
+      },
+      addEffect: function (effect, fn) {
+        var _effect = _effects[effect]
+        if (typeof _effect === 'undefined') {
+          _effects[effect] = fn
+        } else {
+          console.error('this effect has already been existed.')
+        }
+      },
+      removeEffect: function (effect) {
+        var _effect = _effects[effect]
+        if (typeof _effect !== 'undefined') {
+          delete _effects[effect]
+        } else {
+          console.error('this effect does not exists.')
+        }
+      },
+      factory: factory,
+      getEffects: function () {
+        return _effects
+      }
+    }
+  }
+
+  return factory()
+})()
